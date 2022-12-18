bb26015f855460af831eb9b4dc1ae07d3df9984e
ToxicAharen <andreyzer771@gmail.com>
2022-12-18 20:11:36 +0300

    Create ConsoleApplication7.cpp

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+
+struct twice {
+private:
+    double first_;
+    size_t second_;
+public:
+    twice(double first, size_t second) : first_(first), second_(second) {}
+    double cost() {
+        return first_ * second_;
+    }
+};
+
+int main() {
+    twice a(10.5, 10);
+    std::cout << a.cost() << std::endl;
+    return 0;
+}
