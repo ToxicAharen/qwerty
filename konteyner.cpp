3e02b004feeaf6736747e27cc18ed97ca5bb1290
ToxicAharen <andreyzer771@gmail.com>
2022-12-29 17:24:39 +0300

    Create konteyner.cpp

---
+++ konteyner.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+#include <numeric>  
+#include <algorithm>
+using namespace std;
+
+bool greater6(int value)
+{
+    return value > 9;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int x;
+    cout << "Введите Х: ";
+    cin >> x;
+    using namespace std;
+    vector<int> v;
+
+    for (int i = 0; i <= 15; i++)
+    {
+        v.push_back(i);
+    }
+
+    cout << "До замены:\n ( ";
+    for (auto iter = v.begin(); iter != v.end(); iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << ")." << endl;
+
+    
+    replace_if(v.begin(), v.end(), greater6, x);
+
+    cout << "После замены:\n ( ";
+    for (auto iter = v.begin(); iter != v.end(); iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << ").";
+}
