2a3a3386bad88c49b82d7da5d5f819cb33a30e32
ToxicAharen <andreyzer771@gmail.com>
2022-12-30 19:52:27 +0300

    Create para_2.cpp

---
+++ para_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+struct  product{
+private:
+    double first_;
+    size_t second_;
+public:
+    product(double first, size_t second) : first_(first), second_(second) {}
+    double cost() {
+        return first_ * second_;
+    }
+};
+
+int main() {
+    product a(10.75, 50);
+    std::cout << a.cost() << std::endl;
+    return 0;
+}
